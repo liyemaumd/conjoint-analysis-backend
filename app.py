ae380ae8e77dfa17c9eaa68a0c70f2cb072001f5
Liye Ma <liyema@umd.edu>
2025-03-18 21:44:51 -0400

    fix cors

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 #CORS(app)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+#CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+CORS(app, resources={r"/*": {"origins": "https://conjoint-manager-demo.netlify.app"}})
 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
